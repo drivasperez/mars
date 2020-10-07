f9f2e8b25ac617502fe5163618dac8490ca9292d
Daniel Rivas Perez <daniel.rivas@hey.com>
2020-10-07 18:13:51 +0100

    Detect keys with crossterm

--- src/visual/controller.rs
+++ src/visual/controller.rs
@@ -1,29 +1,55 @@
 use crossbeam::channel::Sender;
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 
 pub enum ControllerMessage {
     Paused,
     Close,
 }
 
-pub fn setup_controller(senders: Vec<Sender<ControllerMessage>>) {
-    // for c in stdin() {
-    //     let event = c.expect("Couldn't access stdin event");
+type Senders = Vec<Sender<ControllerMessage>>;
 
-    //     match event {
-    //         Event::Key(Key::Ctrl('c')) => {
-    //             for sender in &senders {
-    //                 sender
-    //                     .send(ControllerMessage::Close)
-    //                     .expect("Couldn't close a thread.");
-    //             }
-    //         }
-    //         _ => {
-    //             for sender in &senders {
-    //                 sender
-    //                     .send(ControllerMessage::Paused)
-    //                     .expect("Couldn't close a thread.");
-    //             }
-    //         }
-    //     };
-    // }
+pub fn setup_controller(senders: Senders) -> crossterm::Result<()> {
+    loop {
+        match read()? {
+            Event::Key(event) => {
+                println!("Detected a key: {:?}", event);
+                if match_key_event(event, &senders) {
+                    break;
+                }
+            }
+            _ => {}
+        };
+    }
+
+    Ok(())
+}
+
+/// If this function returns true, this thread should exit (it got a ctrl-C command).
+fn match_key_event(event: KeyEvent, senders: &Senders) -> bool {
+    match (event.code, event.modifiers) {
+        (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
+            close_all(senders);
+            true
+        }
+        (_, _) => {
+            pause(senders);
+            false
+        }
+    }
+}
+
+fn pause(senders: &Senders) {
+    for sender in senders {
+        sender
+            .send(ControllerMessage::Paused)
+            .expect("Couldn't pause a thread.");
+    }
+}
+
+fn close_all(senders: &Senders) {
+    for sender in senders {
+        sender
+            .send(ControllerMessage::Close)
+            .expect("Couldn't close a thread.");
+    }
 }
